6dafdcae6d55787f43794d702c8eab74ad2ec346
ramon <ramonsilvab05@gmail.com>
2024-07-27 08:57:09 -0300

    feat: adicoionando o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Header from './components/Header'
 import ListaDeContatos from './containers/ListaDeContatos'
 import EstiloGlobal, { Container } from './styles'
 import store from './store'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <ListaDeContatos />
+      </>
+    )
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <Header />
-        <ListaDeContatos />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
